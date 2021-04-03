77edf75c92917d92abbc28bfa3630622e5425de6
Arun Kumar <36177675+ak8017@users.noreply.github.com>
2021-04-02 22:45:42 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,190 @@
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, text
+
+from flask import Flask, jsonify, render_template
+
+
+#################################################
+# Database Setup
+#################################################
+
+engine = create_engine(
+    "postgres://yaqfsxtfrvpnqw:0d59a3e541dd796adf2ef1fbf31c2c0d84c46c59d966db0990cd026ca01894d6@ec2-184-72-235-80.compute-1.amazonaws.com:5432/dbftqqmnch7b9g")
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+def tags_for_the_quote(quote_id):
+    tags = []
+    print(f'getting tags for {quote_id}')
+    tags_result = engine.execute(
+        f'select tag  from tags where quote_id= {quote_id}')
+    for tagrow in tags_result:
+        tags.append(tagrow.tag)
+    return tags
+
+#################################################
+
+
+def quotes_for_author(author_name):
+    result = []
+    print(f'getting quotes for {author_name}')
+    query = text("select id , text from quotes where author_name = :name")
+    quotes_result_set = engine.execute(query, {'name': author_name})
+    for row in quotes_result_set:
+        this_quote = {}
+        this_quote['text'] = row.text
+        this_quote['tags'] = tags_for_the_quote(row.id)
+        result.append(this_quote)
+    return result
+
+#################################################
+
+
+def quotes_for_tag(tag):
+    result = []
+    print(f'getting quotes for {tag}')
+
+    query = text('''select id, text
+            from quotes q inner join tags t on q.id=t.quote_id
+            where t.tag = :tag ''')
+    quotes_result_set = engine.execute(query, {'tag': tag})
+    for row in quotes_result_set:
+        this_quote = {}
+        this_quote['text'] = row.text
+        this_quote['tags'] = tags_for_the_quote(row.id)
+        result.append(this_quote)
+    return result
+
+#################################################
+
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return render_template("index.html")
+
+
+@app.route("/authors")
+def authors():
+    result = {}
+    authors = []
+    author_resuletset = engine.execute(
+        'select name , born , description from author')
+    result['count'] = author_resuletset.rowcount
+
+    for author_row in author_resuletset:
+        this_author = {}
+        quotes = []
+        this_author['name'] = author_row.name
+        this_author['description'] = author_row.description
+        this_author['born'] = author_row.born
+        quotes = quotes_for_author(author_row.name)
+        this_author['count'] = len(quotes)
+        this_author['quotes'] = quotes
+        authors.append(this_author)
+
+    result['details'] = authors
+
+    return jsonify(result)
+
+
+@app.route("/authors/<author_name>")
+def oneauthor(author_name):
+    result = {}
+    query = text(
+        "select name , born , description from author where name = :name")
+    author_result = engine.execute(query, {'name': author_name})
+    # if we found the author, return the details, otherwise return Author not found
+    if(author_result.rowcount == 1):
+        author = author_result.fetchone()
+        result['name'] = author.name
+        result['description'] = author.description
+        quotes = quotes_for_author(author_name)
+        result['quotes'] = quotes
+        result['number_of_quotes'] = len(quotes)
+    else:  # authro not found
+        result['name'] = author_name
+        result['description'] = 'Author not found'
+
+    return jsonify(result)
+
+
+@app.route("/quotes")
+def quotes():
+
+    result = {}
+    result_set = engine.execute('''select id, author_name, text
+    from quotes q inner join author a on q.author_name = a.name
+    order by id''')
+
+    result['total'] = result_set.rowcount
+
+    quotes = []
+    for row in result_set:
+        quote = {}
+        quote['text'] = row.text
+        quote['author'] = row.author_name
+        tags = tags_for_the_quote(row.id)
+        quote['tags'] = tags
+        quotes.append(quote)
+
+    result['quotes'] = quotes
+    return jsonify(result)
+
+
+@app.route("/tags")
+def tags():
+    result = {}
+    tags_result_set = engine.execute('''select tag , count(*) as total from tags
+        group by tag
+        order by total desc''')
+    result['count'] = tags_result_set.rowcount
+    tags = []
+    for row in tags_result_set:
+        this_tag = {}
+        this_tag['name'] = row.tag
+        this_tag['number_of_quotes'] = row.total
+        this_tag['quotes'] = quotes_for_tag(row.tag)
+        tags.append(this_tag)
+    result['details'] = tags
+    return jsonify(result)
+
+
+@app.route("/tags/<tag_name>")
+def onetag(tag_name):
+    result = {}
+    result['tag'] = tag_name
+    quotes = quotes_for_tag(tag_name)
+    result['quotes'] = quotes
+    result['count'] = len(quotes)
+    return jsonify(result)
+
+
+@app.route("/top10tags")
+def top10tags():
+    result = []
+    tags_result_set = engine.execute('''select tag , count(*) as total from tags
+    group by tag
+    order by total desc
+    limit 10''')
+
+    for row in tags_result_set:
+        tag = {}
+        tag['tag'] = row.tag
+        tag['total'] = row.total
+        result.append(tag)
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
